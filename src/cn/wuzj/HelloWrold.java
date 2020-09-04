fd6c2d24680ef13fbcb05c139693657b622f1577
mrl111 <3106732891@qq.com>
2020-09-04 19:00:25 +0800

    first class :helloword

---
+++ src/cn/wuzj/HelloWrold.java
@@ -0,0 +1,13 @@
+package cn.wuzj;
+
+public class HelloWrold {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("helloworld");
+	}
+
+}
